a96e1a73bb0adc04bec3b83b4c39f44a032e46c6
root <fvarlii@gmail.com>
2020-04-17 01:49:25 +0300

    updates

--- myproject/content/admin.py
+++ myproject/content/admin.py
@@ -3,10 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from content.models import Category
 
+from content.models import Content
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
 
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'detail', 'status']
+    list_filter = ['status', 'category']
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Content, ContentAdmin)
--- myproject/content/models.py
+++ myproject/content/models.py
@@ -13,6 +13,26 @@ class Category(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Content(models.Model):
+    STATUS =(
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
     parent = models.ForeignKey('self',blank=True, null=True,related_name='children',on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
